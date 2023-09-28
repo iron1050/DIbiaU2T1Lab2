4e749025e4cedb11eea2b899c282bcdea7d065b1
chukwumad@nycstudents.net <chukwumad@nycstudents.net>
2023-09-28 11:30:03 -0400

    .

--- src/CircleRunner.java
+++ src/CircleRunner.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class CircleRunner {
+public class CircleRunner {
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+        Circle circ1 = new Circle(5);
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+        circ1.printArea();
+        // C. BEFORE running this code, PREDICT what gets printed
+        // I think it will print the area, which is about 78.5
+        // D. run the code and confirm your prediction! Were you correct?
+        // I was correct
+    }
 }
