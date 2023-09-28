0ac440c4b0a56f8fa192bbd4dfc4518120380a1c
chukwumad@nycstudents.net <chukwumad@nycstudents.net>
2023-09-28 12:09:42 -0400

    .

---
+++ src/RectangleRunner.java
@@ -0,0 +1,8 @@
+public class RectangleRunner {
+    public static void main(String[] args) {
+        Rectangle rect1 = new Rectangle(10, 30);
+        rect1.printArea();
+        rect1.printBoxVolume(30);
+
+    }
+}
